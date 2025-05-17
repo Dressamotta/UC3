cc1e15732beb65620a47e262e0d345385435b1ad
Dressamotta <Mottas.andressa@gmail.com>
2025-05-16 21:32:54 -0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'app02'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('contatos/',views.contatos,name='contatos'),
+    # Adicione outras URLs conforme necessário
+
+]
+                                                             
